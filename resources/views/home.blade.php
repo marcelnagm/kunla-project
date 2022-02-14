fdae74ba38479be61e467cf3c1c4bcc740974f6d
Marcel Nagm <marcel.nagm@gmail.com>
2022-02-14 11:58:02 -0400

    homepage desktop criado , secões do tipo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,40 +3,83 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="{{ url('css/home_dsk.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
-    
+
     <body>
         <div class="container">
 
             <div class="row">
                 <div class="col-12 header">
                     <nav class="navbar navbar-expand-lg navbar-light ">
-                        <div class="container-fluid">
-                            <a class="navbar-brand" href="#">                                                               
-                            </a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="#">&lt;Home /></a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">&lt;Formação /></a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">&lt;Contato/></a>
-                                    </li>
-                                    
-                                </ul>
 
-                            </div>
+                        <a class="navbar-brand" href="#">                                                               
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbar-top">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">&lt;Home /></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">&lt;Formação /></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">&lt;Contato/></a>
+                                </li>
+
+                            </ul>
+
                         </div>
                     </nav>
+                    <div class="subtitle">
+                        <span class="ex">&lt; /> </span>Encontre o profissional que precisa_ 
+                    </div>
+                    <div class="search">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <input type="text" placeholder="Cargo ou palavra-chave">                        
+                        <i class="fa-solid fa-location-dot"></i>
+                        <input type="text" placeholder="Estado">                       
+                        <i class="fa-solid fa-dollar-sign"></i>
+                        <input type="text" placeholder="Faixa salarial">
+                        <a type="button" class="btn " >Buscar profissional</a>
+                        
+                    </div>'
                 </div>
+
+
             </div>
 
+        </div>  
+        <div class="row section">  
+        <div class="col-4">  
+            <img src="{{ url('img/dsk/02-desktop.png') }}">
+        </div>  
+        <div class="col-8 section2-texts">  
+            <div class="subtitle-2">
+                Como a <span class="ex2">{</span>Kunla pode ajudar<span class="ex2">}</span> a minha empresa?
+            </div>
+            <div class="text-base-2">
+               Disponibilizamos mapeamentos de profissionais de tecnologia para facilitar seu processo de recrutamento e seleção. É simples e ágil, basta identificar o profissional que deseja e entrar em contato conosco. 
+            </div>
+        </div>  
+        </div>  
+        <div class="row section">  
+        <div class="col-4 section-3-title">
+            
+            Últimos Profissionais 
+que mapeamos
+        </div>  
+        <div class="col-8 section2-texts">  
+            <div class="subtitle-2">
+                Como a <span class="ex2">{</span>Kunla pode ajudar<span class="ex2">}</span> a minha empresa?
+            </div>
+            <div class="text-base-2">
+               Disponibilizamos mapeamentos de profissionais de tecnologia para facilitar seu processo de recrutamento e seleção. É simples e ágil, basta identificar o profissional que deseja e entrar em contato conosco. 
+            </div>
+        </div>  
         </div>  
     <body/>
 </h<html>
